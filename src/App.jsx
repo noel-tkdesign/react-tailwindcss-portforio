ffc7d189476d878ae1c5048f8341cfafa1e3277b
noel-tkdesign <kksdrt@gmail.com>
2024-07-14 09:51:09 +0900

    card section 作成

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,129 @@ function App() {
      </div>
     </div>
    </section>
+   <section className='text-gray-700 border-t border-gray-200'>
+    <div className='container px-5 py-24 mx-auto'>
+      <div className='text-center mb-20'>
+        <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900 '>About Me</h1>
+          <p className='pb-10'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis velit perspiciatis praesentium deserunt tempora saepe odit quam quaerat necessitatibus eius!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab fuga sit expedita dolore, molestias nam harum ipsam eligendi dolorum reiciendis cupiditate quae tempore officiis necessitatibus assumenda hic facere, excepturi magni veritatis laboriosam alias quidem nobis pariatur. Nostrum voluptas excepturi mollitia est pariatur! Impedit aliquam vero sapiente ut cum repellendus tenetur pariatur iure repellat, mollitia sequi veniam obcaecati incidunt, quos maxime officia odit! Quaerat exercitationem consectetur inventore vel laudantium accusamus molestias neque hic amet voluptatibus suscipit, ducimus, cum debitis quo. Asperiores numquam praesentium facere debitis! Assumenda quae odio eligendi ad, voluptates blanditiis quas placeat cumque maiores fuga excepturi consectetur quod in.</p>
+      </div>
+      <div className=' flex flex-wrap'>
+        <div className='md:w-1/3 p-4'>
+          <div className='bg-gray-100 rounded-lg p-8'>
+            <div className='flex items-center mb-3'>
+              <div className='bg-green-500 text-white rounded-full'>
+              <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+               >
+               <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+               </svg>
+              </div>
+              <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                Mobile Application
+              </h2>
+            </div>
+            <div> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, error. Placeat voluptas ducimus similique nostrum corporis mollitia, eos officiis. Illo.
+            </p>
+            <a href="#" className=' flex mt-3 text-green-500 items-center'>More
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+             >
+             <path
+               d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+               fill="currentColor"
+              />
+             </svg>
+            </a>
+            </div>
+          </div>
+        </div>
+        <div className='md:w-1/3 p-4'>
+          <div className='bg-gray-100 rounded-lg p-8'>
+            <div className='flex items-center mb-3'>
+              <div className='bg-green-500 text-white rounded-full'>
+              <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+               >
+               <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+               </svg>
+              </div>
+              <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                Web Design
+              </h2>
+            </div>
+            <div> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, error. Placeat voluptas ducimus similique nostrum corporis mollitia, eos officiis. Illo.
+            </p>
+            <a href="#" className=' flex mt-3 text-green-500 items-center'>More
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+             >
+             <path
+               d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+               fill="currentColor"
+              />
+             </svg>
+            </a>
+            </div>
+          </div>
+        </div>
+        <div className='md:w-1/3 p-4'>
+          <div className='bg-gray-100 rounded-lg p-8'>
+            <div className='flex items-center mb-3'>
+              <div className='bg-green-500 text-white rounded-full'>
+              <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+               >
+               <path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" />
+               </svg>
+              </div>
+              <h2 className='text-gray-900 text-lg font-medium ml-2'>
+                Web Aplication
+              </h2>
+            </div>
+            <div> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, error. Placeat voluptas ducimus similique nostrum corporis mollitia, eos officiis. Illo.
+            </p>
+            <a href="#" className=' flex mt-3 text-green-500 items-center'>More
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+             >
+             <path
+               d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+               fill="currentColor"
+              />
+             </svg>
+            </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   </section>
    </>
   );
 }
