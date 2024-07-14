7969f82699d8a4215e6a159b5c1b5193971a173f
noel-tkdesign <kksdrt@gmail.com>
2024-07-14 17:58:11 +0900

    second section 作成

--- src/App.jsx
+++ src/App.jsx
@@ -168,6 +168,46 @@ function App() {
     </div>
 
    </section>
+   <section className='text-gray-700 border-t border-gray-200'>
+    <div className='container px-5 py-24 mx-auto flex flex-wrap'>
+      {/* left side*/}
+      <div className='mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center'>
+        <img src="public/img/pc.jpg" alt="" className='rounded'/>
+      </div>
+      {/* right side*/}
+      <div className='lg:pl-12 lg;py-6 w-full lg:w-1/2'>
+        <h1 className='text-2xl sm:text-3xl font-medium text-gray-900 mb-10 text-center lg:text-left'>
+          My Skills
+          </h1>
+        <div className='w-full'>
+          <h2>SwiftUI</h2>
+          <div className='shadow bg-green-100 mt-2 w-full mb-10' >
+          <div className='bg-green-600 text-xs leading-none py-1 text-center text-white 'style={{width:"85%"}}>
+            85%
+            </div>
+          </div>
+          <h2>Flutter</h2>
+          <div className='shadow bg-green-100 mt-2 w-full mb-10'>
+          <div className='bg-green-600 text-xs leading-none py-1 text-center text-white 'style={{width:"70%"}}>
+            70%
+            </div>
+          </div>
+          <h2>Android</h2>
+          <div className='shadow bg-green-100 mt-2 w-full mb-10'>
+          <div className='bg-green-600 text-xs leading-none py-1 text-center text-white 'style={{width:"60%"}}>
+            60%
+            </div>
+          </div>
+          <h2>React</h2>
+          <div className='shadow bg-green-100 mt-2 w-full mb-10'>
+          <div className='bg-green-600 text-xs leading-none py-1 text-center text-white 'style={{width:"50%"}}>
+            50%
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
    </>
   );
 }
